c6d003c2a97cb671977535ecde52a77b706dbb5b
ttp0513 <johnphanthe@gmail.com>
2025-10-12 20:19:53 -0400

    HW6 Traditional Function completed

--- LetPhilMentorship-wave-5/practice-homework/mini-project-Functions/Traditional-Functions/Mentee/script.js
+++ LetPhilMentorship-wave-5/practice-homework/mini-project-Functions/Traditional-Functions/Mentee/script.js
@@ -1,11 +1,23 @@
 // Mentee Version — Steps Only (same steps appear in Mentor)
 
 // STEP 1 — Create a function named add that takes two numbers and returns the sum.
+function add(a, b) {
+    return a + b};
 
 // STEP 2 — Create a function named greet that takes a name and returns the string 'Hello, <name>!'
+function greet(name) {
+    return `Hello, ${name}!`}
 
 // STEP 3 — Create a function named isAdult that takes an age and returns true if age >= 18, otherwise false.
+function isAdult(age) {
+    return age >= 18;}
 
 // STEP 4 — Create a function named maxOfThree that returns the largest of three numbers.
+function maxOfThree(a, b, c) {
+    return Math.max(a, b, c);}
 
 // STEP 5 — TEST: Log each function call result to the console with sample inputs.
+console.log(add(1,2));
+console.log(greet("John"));
+console.log(isAdult(20));
+console.log(maxOfThree(5,10,3));    
