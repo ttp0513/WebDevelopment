2e4b28e18eb7643c97ef39588c13b7d6bfbd2652
ttp0513 <johnphanthe@gmail.com>
2025-10-24 14:18:15 -0400

    Update nav bar styles

--- LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
+++ LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
@@ -1,6 +1,7 @@
-// Add Event Listeners to Menu Bar Icon
+// Add Event Listeners to Menu Bar Icon to change hamburger menu to cross menu
 let menuBar = document.querySelector(".menu-bar"); 
 
 menuBar.addEventListener('click', () => {
     menuBar.classList.toggle('active');
+    document.querySelector('.menu-items').classList.toggle('active');
 });
