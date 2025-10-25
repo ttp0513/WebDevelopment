b4d9088c1bced7d1fe77f5250edc00a1722e07f9
ttp0513 <johnphanthe@gmail.com>
2025-10-24 21:40:19 -0400

    Add and style hero-image

--- LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
+++ LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
@@ -6,20 +6,4 @@ menuBar.addEventListener('click', () => {
     document.querySelector('.nav-items').classList.toggle('active');
 });
 
-// // Function to update time and date every second
-// function updateTimeAndDate() {
-//     const timeDisplay = document.getElementById('time-display');
-//     const dateDisplay = document.getElementById('date-display');
-//     const now = new Date();
-//     const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
-//     const timeString = now.toLocaleTimeString([], options);
-//     const dateString = now.toLocaleDateString();
-//     timeDisplay.textContent = timeString;
-//     dateDisplay.textContent = dateString;
-// }
-
-// // Update time and date every second
-// setInterval(updateTimeAndDate, 1000);
-// // Initial call to display time and date immediately on load
-// updateTimeAndDate();
 
