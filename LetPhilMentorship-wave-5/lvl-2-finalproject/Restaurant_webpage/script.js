ca94983489c181e673b8d36fb51115a4f7618034
ttp0513 <johnphanthe@gmail.com>
2025-10-24 20:48:11 -0400

    Update the nav styles

--- LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
+++ LetPhilMentorship-wave-5/lvl-2-finalproject/Restaurant_webpage/script.js
@@ -1,7 +1,25 @@
 // Add Event Listeners to Menu Bar Icon to change hamburger menu to cross menu
-let menuBar = document.querySelector(".menu-bar"); 
+let menuBar = document.querySelector(".nav-bar"); 
 
 menuBar.addEventListener('click', () => {
     menuBar.classList.toggle('active');
-    document.querySelector('.menu-items').classList.toggle('active');
+    document.querySelector('.nav-items').classList.toggle('active');
 });
+
+// // Function to update time and date every second
+// function updateTimeAndDate() {
+//     const timeDisplay = document.getElementById('time-display');
+//     const dateDisplay = document.getElementById('date-display');
+//     const now = new Date();
+//     const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+//     const timeString = now.toLocaleTimeString([], options);
+//     const dateString = now.toLocaleDateString();
+//     timeDisplay.textContent = timeString;
+//     dateDisplay.textContent = dateString;
+// }
+
+// // Update time and date every second
+// setInterval(updateTimeAndDate, 1000);
+// // Initial call to display time and date immediately on load
+// updateTimeAndDate();
+
